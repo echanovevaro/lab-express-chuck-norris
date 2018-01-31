702a8a424ac07d547bb69876d13ac8c8e5c5eeb2
Alvaro <echanovevaro@gmail.com>
2018-01-31 20:46:48 +0100

    segunda iteracion sin links

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,30 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+ app.get('/random', (req, res) => {
+    client.getRandomJoke()
+        .then((response) => {
+            res.render('random', {joke: response.value});
+        }).catch((err) => {
+            console.error(err.message);
+        });
+ });
+
+ app.get('/categories', (req, res) => {
+    client.getJokeCategories()
+        .then((response)=>  {
+            res.render('categories', {categories: response});
+        })
+        .catch((err)=> {
+            console.error(err.message);
+        });
+ });
+
+ app.listen(3000, () => {
+    console.log('My first app listening on port 3000!')
+});
+
